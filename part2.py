096a8ea9020036c05ab516e11ae1e9f9b5017868
Raylessss <rayless010619@gmail.com>
2025-09-26 00:35:12 +0800

    added total price

--- part2.py
+++ part2.py
@@ -18,6 +18,7 @@ def main():
     print("0. Finish Order")
 
     order_list = []
+    total_food_price = 0.0
 
     while True:
             choice = int(input("\nSelect food item (1-5) or 0 to finish: "))
@@ -26,24 +27,31 @@ def main():
                 break
             elif choice == 1:
                 order_list.append("Nasi Goreng USA")
+                total_food_price += 11.50
                 print("Added Nasi Goreng USA")
             elif choice == 2:
                 order_list.append("Burger Ayam Crispy")
+                total_food_price += 6.00
                 print("Added Burger Ayam Crispy")
             elif choice == 3:
                 order_list.append("Nasi Lemak Ayam")
+                total_food_price += 8.00
                 print("Added Nasi Lemak Ayam")
             elif choice == 4:
                 order_list.append("Extra Joss Mangga")
+                total_food_price += 3.50
                 print("Added Extra Joss Mangga")
             elif choice == 5:
                 order_list.append("Teh Tarik")
+                total_food_price += 2.80
                 print("Added Teh Tarik")
             elif choice == 6:
                 order_list.append("French Fries")
+                total_food_price += 4.80
                 print("Added French Fries")
             elif choice == 7:
                 order_list.append("Cheese Leleh")
+                total_food_price += 12.00
                 print("Added Cheese Leleh")
             else:
                 print("Invalid choice, please try again.")
