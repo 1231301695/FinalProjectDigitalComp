80c792076a14c7e1e7c1a205f358e368ef011cb4
1231301695 <1231301695@student.mmu.edu.my>
2025-09-26 00:08:40 +0800

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,19 +1,8 @@
 import random
 
 def main():
-    print("Welcome to Grab Food!")
-    print("-"*40)
-
-    user_name = input("Name: ")
-    address = input("Address: ")
-
-    menu = {
-        1: ("Nasi Goreng USA", 11.50),
-        2: ("Burger Ayam Crispy", 6.00),
-        3: ("Nasi Lemak Ayam", 8.00),
-        4: ("Extra Joss Mangga", 3.50),
-        5: ("Teh Tarik", 2.80),
-        6: ("French Fries", 4.80),
-        7: ("Cheese Leleh", 12.00)
-    }
+    print("Welcome to Grab Food Delivery!")
+    print("-" * 40)
     
+    user_name = input("Enter your name: ")
+    address = input("Enter your delivery address: ")
