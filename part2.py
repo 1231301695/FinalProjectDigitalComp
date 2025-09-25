bdaaf2236d44a2e7e5421c0f107c72ad15883180
LWF4237 <lwf4237@gmail.com>
2025-09-26 00:12:14 +0800

    Update part2.py

--- part2.py
+++ part2.py
@@ -6,3 +6,13 @@ def main():
     
     user_name = input("Enter your name: ")
     address = input("Enter your delivery address: ")
+
+    print("\nMenu:")
+    print("1. Nasi Goreng USA - RM11.50")
+    print("2. Burger Ayam Crispy - RM6.00")
+    print("3. Nasi Lemak Ayam - RM8.00")
+    print("4. Extra Joss Mangga - RM3.50")
+    print("5. Teh Tarik - RM2.80")
+    print("6. French Fries - RM4.80")
+    print("7. Cheese Leleh - RM12.00")
+    print("0. Finish Order")
