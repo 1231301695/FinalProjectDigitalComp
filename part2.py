2c4de51db26d481e3315a61e7df401e06fb28a3a
Ahmad Hafriz <1231302688@student.mmu.edu.my>
2025-09-26 00:20:44 +0800

    add switch case

--- part2.py
+++ part2.py
@@ -16,3 +16,34 @@ def main():
     print("6. French Fries - RM4.80")
     print("7. Cheese Leleh - RM12.00")
     print("0. Finish Order")
+
+    order_list = []
+
+    while True:
+            choice = int(input("\nSelect food item (1-5) or 0 to finish: "))
+
+            if choice == 0:
+                break
+            elif choice == 1:
+                order_list.append("Nasi Goreng USA")
+                print("Added Nasi Goreng USA")
+            elif choice == 2:
+                order_list.append("Burger Ayam Crispy")
+                print("Added Burger Ayam Crispy")
+            elif choice == 3:
+                order_list.append("Nasi Lemak Ayam")
+                print("Added Nasi Lemak Ayam")
+            elif choice == 4:
+                order_list.append("Extra Joss Mangga")
+                print("Added Extra Joss Mangga")
+            elif choice == 5:
+                order_list.append("Teh Tarik")
+                print("Added Teh Tarik")
+            elif choice == 6:
+                order_list.append("French Fries")
+                print("Added French Fries")
+            elif choice == 7:
+                order_list.append("Cheese Leleh")
+                print("Added Cheese Leleh")
+            else:
+                print("Invalid choice, please try again.")
